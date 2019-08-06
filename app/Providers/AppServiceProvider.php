31e9af242f1f1d79b2152d41881883295f13e939
GRS <salazar.g.guillermo@gmail.com>
2019-08-06 17:36:40 -0500

    Implementacion Admin LTE

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+//use Illuminate\View\View;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view::share('theme', 'lte');
+    }
+
     /**
      * Register any application services.
      *
@@ -16,13 +28,5 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+    
 }
