055e4f73051f48ecc6dfb65282dde1c7a93010f9
GRS <salazar.g.guillermo@gmail.com>
2019-08-09 17:02:27 -0500

    Validacion Cliente

--- resources/views/admin/menu/crear.blade.php
+++ resources/views/admin/menu/crear.blade.php
@@ -5,6 +5,10 @@
     Sistema Menus
 @endsection
 
+@section('scripts')
+    <script src="{{asset("assets/pages/scripts/admin/crear.js")}}" type="text/javascript"></script>
+@endsection
+
 @section('contenido')
     <div class="row">
         <div class="col-lg-12"> 
--- resources/views/admin/menu/form.blade.php
+++ resources/views/admin/menu/form.blade.php
@@ -3,7 +3,7 @@
 
         <div class="col-lg-8">
           <!-- old('') REPOBLA por Validacion-->
-        <input type="text" name="nombre" id="nombre" class="form-control" value="{{old('nombre')}}" />
+        <input type="text" name="nombre" id="nombre" class="form-control" value="{{old('nombre')}}" required/>
         </div>
 </div>
 
@@ -11,7 +11,7 @@
         <label for="url" class="col-lg-3 control-label requerido">URL</label>
 
         <div class="col-lg-8">
-          <input type="text" name="url" id="url" class="form-control" value="{{old('url')}}" />
+          <input type="text" name="url" id="url" class="form-control" value="{{old('url')}}" required/>
         </div>
 </div>
 
@@ -20,5 +20,9 @@
 
         <div class="col-lg-8">
           <input type="text" name="icono" id="icono" class="form-control" value="{{old('icono')}}" />
+        </div>
+
+        <div class="col-lg-1">
+            <span id="mostrar-icono" class="fa fa-fw {{old("icono")}}"></span>
         </div>
 </div>
